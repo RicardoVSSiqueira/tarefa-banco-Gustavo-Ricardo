58da4e1e00a71e61e9f6455fbb180eaead112055
gustaeps2004 <130726520+gustaeps2004@users.noreply.github.com>
2026-08-13 20:37:57 -0300

    feat: adicionando atividade 2 completa

--- consultas_nexashop.sql
+++ consultas_nexashop.sql
@@ -1,5 +1,4 @@
 USE ecommerce_nexashop;
-
 SELECT
 'clientes' AS tabela, COUNT(*) AS total FROM clientes
 UNION ALL
@@ -11,7 +10,6 @@ SELECT
 UNION ALL
 SELECT
 'avaliacoes' AS tabela, COUNT(*) AS total FROM avaliacoes;
-
 
 -- TAREFA 2.1
 
@@ -27,4 +25,66 @@ WHERE
 AND estado IN ('SC', 'PR', 'RS')
 ORDER BY
 	estado,
-    nome
+    nome;
+
+    
+-- TAREFA 2.2
+    
+SELECT 
+	*
+FROM
+	clientes
+WHERE
+	nome like '%gustavo%';
+
+
+-- TAREFA 2.3
+
+SELECT
+	nome,
+    email AS 'e-mail',
+    cidade,
+    estado
+FROM
+	clientes
+WHERE
+	telefone IS NULL;
+    
+    
+-- TAREFA 2.4
+
+SELECT 
+	*
+FROM
+	pedidos
+WHERE
+	(valor_total BETWEEN 100 AND 500)
+AND status = 'Aprovado'
+ORDER BY
+	valor_total
+DESC;
+
+-- TAREFA 2.5
+
+SELECT 
+	nome,
+    categoria,
+    estoque
+FROM
+	produtos
+WHERE
+	estoque < 10
+AND ativo IS TRUE
+ORDER BY
+	estoque;
+
+-- TAREFA 2.6
+
+SELECT 
+	id,
+    valor_total,
+    cupom_desconto
+FROM
+	pedidos
+WHERE
+	cupom_desconto IS NOT NULL;
